a6d79032be375453737ff0ea4952a88199fa1a9e
X901 <Basil@macbook-pro-2.local>
2017-03-31 20:33:48 +0300

    Initial Commit

---
+++ AnimationMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationMenu
+//
+//  Created by X901 on 3/31/17.
+//  Copyright © 2017 Basil Baragabah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
